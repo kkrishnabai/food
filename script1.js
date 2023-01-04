f6b6f000810e359cc4c3c4da778c3254dca320b0
kkrishnabai <84125692+kkrishnabai@users.noreply.github.com>
2023-01-04 16:06:48 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+
+function loadvalues()
+{
+    document.getElementById("id1").innerHTML = "Hello World";
+    document.getElementById("id2").innerHTML = "Hello World";
+    document.getElementById("id3").innerHTML = "Hello World";
+
+}
